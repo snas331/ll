6f2a2e5d3cfb642ab195d914aef9ed996e3e7d11
yakov23 <yakovoberderfer13@gmail.com>
2023-12-12 13:54:13 +0300

    files ignored

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int initialAccount = 110;
+        int replenishment = 1100;
+        int bonus;
+
+        if (replenishment >= 1100) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
+
+
+    }
+}
