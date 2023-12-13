5d8940f52bdd67a54afbd0b3c9c5089ac849d42c
yakov23 <yakovoberderfer13@gmail.com>
2023-12-13 11:10:23 +0300

    cheanged dz

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,25 @@ public class Main {
         int initialAccount = 110;
         int replenishment = 1100;
         int bonus;
+        int calculatedBonus = 11;
+        int replenishmentTwo = 1050;
+        int calculatedBonusTwo =10;
+        int result = initialAccount + replenishment + calculatedBonus;
+        int resultTwo = initialAccount + replenishmentTwo + calculatedBonusTwo;
+
+        System.out.println(result);
 
         if (replenishment >= 1100) {
             bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
+
+        System.out.println(resultTwo);
+
+        if (replenishment >= 1050) {
+            bonus = replenishmentTwo / 100;
         } else {
             bonus = 0;
         }
